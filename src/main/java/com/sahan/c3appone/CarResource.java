4bce3731bdfe35949c41cf97cc40ad87ede7a980
Sahan Perera <sahanyuganthamail@gmail.com>
2021-05-23 13:40:20 +0530

    update 23052021 new

--- src/main/java/com/sahan/c3appone/CarResource.java
+++ src/main/java/com/sahan/c3appone/CarResource.java
@@ -9,20 +9,20 @@ import com.google.gson.Gson;
 import com.sahan.c3appone.dao.CarDao;
 import com.sahan.c3appone.model.Car;
 
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.PathParam;
-import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.core.MediaType;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 @Path("cars")
 public class CarResource {
 	
 	private Logger logger = LogManager.getLogger(CarResource.class);
 
-	@GET
+	@javax.ws.rs.GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public String getCars() {
 		
--- src/main/java/com/sahan/c3appone/config/API_URLs.java
+++ src/main/java/com/sahan/c3appone/config/API_URLs.java
@@ -3,6 +3,6 @@ package com.sahan.c3appone.config;
 public class API_URLs {
 	
 	//public static final String CAR_API = "http://localhost:8080/c3appone/api/cars";
-	public static final String CAR_API = "http://https://cohort-03-app.herokuapp.com/api/cars";
+	public static final String CAR_API = "https://cohort-03-app.herokuapp.com/api/cars";
 
 }

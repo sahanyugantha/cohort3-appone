a0d49b05e9dae3934831f06a329a9c4eda7857d2
Sahan Perera <sahanyuganthamail@gmail.com>
2021-05-23 13:15:55 +0530

    update 23052021

--- src/main/java/com/sahan/c3appone/config/API_URLs.java
+++ src/main/java/com/sahan/c3appone/config/API_URLs.java
@@ -2,6 +2,7 @@ package com.sahan.c3appone.config;
 
 public class API_URLs {
 	
-	public static final String CAR_API = "http://localhost:8080/c3appone/api/cars";
+	//public static final String CAR_API = "http://localhost:8080/c3appone/api/cars";
+	public static final String CAR_API = "http://https://cohort-03-app.herokuapp.com/api/cars";
 
 }

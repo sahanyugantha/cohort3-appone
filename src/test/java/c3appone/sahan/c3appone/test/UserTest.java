351f35ebc89be4e47f774d8a0203cc53e69bbc71
Sahan Perera <sahanyuganthamail@gmail.com>
2021-05-23 12:40:31 +0530

    update 23052021

--- src/test/java/c3appone/sahan/c3appone/test/UserTest.java
+++ src/test/java/c3appone/sahan/c3appone/test/UserTest.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import org.junit.jupiter.api.Test;
-import org.junit.rules.ExpectedException;
 
 import com.sahan.c3appone.dao.UserDao;
 import com.sahan.c3appone.model.User;
